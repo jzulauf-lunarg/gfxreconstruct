977a426b343c9c5caed09f840540620020f27736
Mark Young <marky@lunarg.com>
2024-11-05 17:00:33 -0500

    codegen: Migrate Json consumer body to endFile
    Move the Vulkan JSON consumer generation from generate_feature to
    endFile time.

--- framework/generated/khronos_generators/vulkan_generators/vulkan_json_consumer_body_generator.py
+++ framework/generated/khronos_generators/vulkan_generators/vulkan_json_consumer_body_generator.py
@@ -119,33 +119,15 @@ class VulkanExportJsonConsumerBodyGenerator(BaseGenerator):
 
     def endFile(self):
         """Method override."""
-        body = format_cpp_code('''
-            GFXRECON_END_NAMESPACE(decode)
-            GFXRECON_END_NAMESPACE(gfxrecon)
-        ''')
-        write(body, file=self.outFile)
-
-        # Finish processing in superclass
-        BaseGenerator.endFile(self)
-
-    def need_feature_generation(self):
-        """Indicates that the current feature has C++ code to generate."""
-        if self.feature_cmd_params:
-            return True
-        return False
-
-    def generate_feature(self):
-        """Performs C++ code generation for the feature."""
-
         # TODO: Each code generator is passed a blacklist like framework\generated\vulkan_generators\blacklists.json
         # of functions and structures not to generate code for. Once the feature is implemented, the following can be
         # replaced with adding vkCreateRayTracingPipelinesKHR in corresponding blacklist.
         if 'vkCreateRayTracingPipelinesKHR' in self.APICALL_BLACKLIST:
             self.APICALL_BLACKLIST.remove('vkCreateRayTracingPipelinesKHR')
 
-        for cmd in self.get_filtered_cmd_names():
+        for cmd in self.get_all_filtered_cmd_names():
             if not cmd in self.customImplementationRequired:
-                info = self.feature_cmd_params[cmd]
+                info = self.all_cmd_params[cmd]
                 return_type = info[0]
                 values = info[2]
 
@@ -168,6 +150,21 @@ class VulkanExportJsonConsumerBodyGenerator(BaseGenerator):
                     }
                 ''', 1)
                 write(cmddef, file=self.outFile)
+
+        body = format_cpp_code('''
+            GFXRECON_END_NAMESPACE(decode)
+            GFXRECON_END_NAMESPACE(gfxrecon)
+        ''')
+        write(body, file=self.outFile)
+
+        # Finish processing in superclass
+        BaseGenerator.endFile(self)
+
+    def need_feature_generation(self):
+        """Indicates that the current feature has C++ code to generate."""
+        if self.feature_cmd_params:
+            return True
+        return False
 
     def is_command_buffer_cmd(self, command):
         if 'vkCmd' in command:
