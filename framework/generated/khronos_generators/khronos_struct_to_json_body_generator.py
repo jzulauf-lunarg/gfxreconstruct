5e976f6cf50c9a8b6a73d435ed0842f14575bddb
Mark Young <marky@lunarg.com>
2024-11-06 16:35:35 -0500

    codegen: Update extension struct handling
    This change modifies how the extension structs are stored to be more
    obvious.  Previously, the extension structs were stored by the
    struct that did the extending.  Instead, switch to storing the extended
    structs based on the parent that is extended.
    
    This resulted in order changes in output of the various pNext structs
    so sort them so they will no longer change in order.
    
    ** Warning **
    It does re-arrange items in the file, but they are all still present.
    *************

--- framework/generated/khronos_generators/khronos_struct_to_json_body_generator.py
+++ framework/generated/khronos_generators/khronos_struct_to_json_body_generator.py
@@ -215,7 +215,14 @@ class KhronosStructToJsonBodyGenerator():
     def makeExtendedStructBody(self):
         body = ''
         var_name = self.getExtendedStructVarName().lower()
+
+        extended_list = []
         for struct in self.all_extended_structs:
+            for ext_struct in self.all_extended_structs[struct]:
+                if ext_struct not in extended_list and ext_struct not in self.all_struct_aliases:
+                    extended_list.append(ext_struct)
+
+        for struct in sorted(extended_list):
             if struct not in self.struct_type_names:
                 continue
             body += '''
