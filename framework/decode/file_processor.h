99848100e238ac37db0d9cc03f8f4e52a73f1f7d
John Zulauf <jzulauf@lunarg.com>
2025-09-05 14:00:19 -0600

    Rewrite preload loop to avoid a ProcessBlocks variant
    Also change data accumulation from N-squared to N

--- framework/decode/file_processor.h
+++ framework/decode/file_processor.h
@@ -30,12 +30,14 @@
 #include "decode/api_decoder.h"
 #include "util/compressor.h"
 #include "util/defines.h"
+#include "util/logging.h"
 #include "util/file_input_stream.h"
 
 #include <algorithm>
 #include <cstdint>
 #include <cstdio>
 #include <deque>
+#include <functional>
 #include <string>
 #include <unordered_map>
 #include <utility>
@@ -129,24 +131,26 @@ class FileProcessor
         block_index_to_          = block_index_to;
     }
 
+    bool IsFrameDelimiter(format::BlockType block_type, format::MarkerType marker_type) const;
+    bool IsFrameDelimiter(format::ApiCallId call_id) const;
+
   protected:
+    bool DoProcessNextFrame(const std::function<bool()>& block_processor);
+    bool ProcessBlocksOneFrame();
+
     bool ContinueDecoding();
 
     bool ReadBlockHeader(format::BlockHeader* block_header);
 
     virtual bool ReadBytes(void* buffer, size_t buffer_size);
 
-    bool SkipBytes(size_t skip_size);
+    virtual bool SkipBytes(size_t skip_size);
 
     bool ProcessFunctionCall(const format::BlockHeader& block_header, format::ApiCallId call_id, bool& should_break);
 
     bool ProcessMethodCall(const format::BlockHeader& block_header, format::ApiCallId call_id, bool& should_break);
 
     bool ProcessMetaData(const format::BlockHeader& block_header, format::MetaDataId meta_data_id);
-
-    bool IsFrameDelimiter(format::BlockType block_type, format::MarkerType marker_type) const;
-
-    bool IsFrameDelimiter(format::ApiCallId call_id) const;
 
     void HandleBlockReadError(Error error_code, const char* error_message);
 
@@ -158,6 +162,8 @@ class FileProcessor
     bool ProcessAnnotation(const format::BlockHeader& block_header, format::AnnotationType annotation_type);
 
     void PrintBlockInfo() const;
+
+    bool HandleBlockEof(const char* operation, bool report_frame_and_block);
 
   protected:
     uint64_t                 current_frame_number_;
@@ -318,8 +324,7 @@ class FileProcessor
   private:
     ActiveFileContext& GetCurrentFile()
     {
-        assert(file_stack_.size());
-
+        GFXRECON_ASSERT(file_stack_.size());
         return file_stack_.back();
     }
 };
